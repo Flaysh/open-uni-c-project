63b3de06071d35141af6b92d95676ae55b2b965a
flaysh <Itayf3x@gmail.com>
2021-03-12 17:15:12 +0200

    added CLion data for refactor.

---
+++ cmake-build-debug/CMakeFiles/Flayshhhh.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/flaysh/Projects/Compare-C-Projects/C_finished_project/firstTransition.c" "/home/flaysh/Projects/Compare-C-Projects/C_finished_project/cmake-build-debug/CMakeFiles/Flayshhhh.dir/firstTransition.c.o"
+  "/home/flaysh/Projects/Compare-C-Projects/C_finished_project/main.c" "/home/flaysh/Projects/Compare-C-Projects/C_finished_project/cmake-build-debug/CMakeFiles/Flayshhhh.dir/main.c.o"
+  "/home/flaysh/Projects/Compare-C-Projects/C_finished_project/secondTransition.c" "/home/flaysh/Projects/Compare-C-Projects/C_finished_project/cmake-build-debug/CMakeFiles/Flayshhhh.dir/secondTransition.c.o"
+  "/home/flaysh/Projects/Compare-C-Projects/C_finished_project/utility.c" "/home/flaysh/Projects/Compare-C-Projects/C_finished_project/cmake-build-debug/CMakeFiles/Flayshhhh.dir/utility.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Flayshhhh.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Flayshhhh.dir/firstTransition.c.o"
+  "CMakeFiles/Flayshhhh.dir/main.c.o"
+  "CMakeFiles/Flayshhhh.dir/secondTransition.c.o"
+  "CMakeFiles/Flayshhhh.dir/utility.c.o"
+  "Flayshhhh"
+  "Flayshhhh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Flayshhhh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
